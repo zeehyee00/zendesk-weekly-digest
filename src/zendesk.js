c1daba5dbb7fe03be4e460a9f37956a713ec8524
zeehyee00 <wlgp9324@ezcom.co.kr>
2026-09-11 15:52:32 +0900

    feat: con-ezcom-dev 샌드박스 헬프센터에 draft article 생성 함수 추가

--- src/zendesk.js
+++ src/zendesk.js
@@ -1,6 +1,24 @@
 import 'dotenv/config';
 import { filterLastWeekArticles } from './filterArticles.js';
 
+async function getAccessToken() {
+    const url = `https://${process.env.ZENDESK_SUBDOMAIN}.zendesk.com/oauth/tokens`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            grant_type: 'client_credentials',
+            client_id: process.env.ZENDESK_CLIENT_ID,
+            client_secret: process.env.ZENDESK_CLIENT_SECRET,
+            scope: 'read users:read hc:read hc:write',
+        }),
+    });
+
+    const data = await response.json();
+    return data.access_token;
+}
+
 async function getSourceArticles(sectionId) {
     const url = `${process.env.ZENDESK_PUBLIC_BASE}/api/v2/help_center/sections/${sectionId}/articles.json`;
 
@@ -11,3 +29,25 @@ async function getSourceArticles(sectionId) {
 
 }
 
+async function createDraftArticle(token, sectionId, title, bodyHtml) {
+    const url = `https://${process.env.ZENDESK_SUBDOMAIN}.zendesk.com/api/v2/help_center/sections/${sectionId}/articles.json`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            article: {
+                title: title,
+                body: bodyHtml,
+                draft: true,
+                locale: 'ko'
+            },
+        }),
+    });
+
+    const data = await response.json();
+    return data.article;
+}
